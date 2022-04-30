f9d4b4972cc8b4834dd8352a1d920fc949fbb8a8
crawen <ardaseyhantemli1@gmail.com>
2022-04-30 13:32:47 +0000

    import

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+import React from "react";
+import logo from "./xasza.png";
+
+function App() {
+  const [data, setData] = React.useState(null);
+  
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  React.useEffect(() => {
+   document.title = "CrawenOS"
+}, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
