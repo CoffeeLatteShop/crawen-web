c1ae8ff2c0ba9e883c3c3922d3bfb22b693b1b47
sertchan <ardaseyhantemli1@gmail.com>
2022-05-02 22:20:57 +0300

    Added Header & Improved Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import './navbar.css';
 
-import {RiCloseLin, RiMenu3Line} from 'react-icons/ri'
+import React, {useState} from 'react';
+import {RiCloseLin, RiCloseLine, RiMenu3Line} from 'react-icons/ri'
 
-import React from 'react';
 import logo from '../../assests/karma.png'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='crawenOS__navbar'>
       <div className='crawenOS__navbar-links'>
@@ -16,6 +17,26 @@ const Navbar = () => {
           <p><a href="#Information">Information</a></p>
           <p><a href="#Features">Features</a></p>
           <p><a href="#Developers">Developers</a></p>
+      </div>
+      <div className="crawenOS__navbar-download">
+        <button class="glow-on-hover" type="button">Download</button>
+      </div>
+      <div className="crawenOS__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="crawenOS__navbar-menu_container scale-up-center">
+          <div className="crawenOS__navbar-menu_container-links">
+          <p><a href="#Information">Information</a></p>
+          <p><a href="#Developers">Developers</a></p>
+          <p><a href="#Features">Features</a></p>
+          </div>
+         <div className="crawenOS__navbar-download">
+           <button class="glow-on-hover" type="button">Download</button>
+          </div>
+        </div>
+        )}
       </div>
       </div>
     </div>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,21 @@
 import './header.css';
 
 import React from 'react';
+import mockup from '../../assests/crawen-mockup-pc1.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="crawenOS__header section__padding" id="Information">
+      <div className='crawenOS__header-content'>
+        <h1 className="gradient__text">
+          Yeah It's What You Did Expect.
+        </h1>
+        <p>We started to design our own custom windows operating system. We are designing an OS that will make people feel safer and at the same time not need to do much. People who set up our system will not need to search for videos on youtube such as performance enhancement/upgrade. It will be a beautiful website, open to all. We are improving ourselves to support more and take firm steps. We plan to publish the toolbox and updates as open source in the updates we will make. In this way, I believe that we can get your help, too..</p>
+      </div>
+      <div className="crawenOS__header-image"> 
+        <img src={mockup} alt='mockup'/>
+      </div>
+    </div>
   )
 }
 
